adc1e27a4f8a06b746e3eda70a0903144f6ad028
Philipp Memmel <philipp.memmel@gmail.com>
2025-03-12 14:59:06 +0100

    MDL-84844 core_course: Add index to course_sections for mod_subsection

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024100702.07;              // 20241007      = branching date YYYYMMDD - do not modify!
+$version  = 2024100702.08;              // 20241007      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '4.5.2+ (Build: 20250311)';    // Human-friendly version name
